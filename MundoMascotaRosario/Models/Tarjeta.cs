943577c9db1034cb1f394248b341b6b0c8b26da7
juanpettinari <juan.pettinari@gmail.com>
2016-07-18 00:24:39 -0300

    comm

--- MundoMascotaRosario/Models/Tarjeta.cs
+++ MundoMascotaRosario/Models/Tarjeta.cs
@@ -16,11 +16,44 @@ namespace MundoMascotaRosario.Models
 
         public string TipoDeTarjeta { get; set; }
         public int NumeroTarjeta { get; set; }
-        public int MesExpiracion { get; set; }
-        public int AnoExpiracion { get; set; }
+        public MesExpiracion MesExpiracion { get; set; }
+        public AnoExpiracion AnoExpiracion { get; set; }
         public string Titular { get; set; }
         public string CodigoSeguridad { get; set; }
+        public EstadoTarjeta Estado { get; set; }
 
         public virtual ICollection<OrdenDeCompra> OrdenesDeCompra { get; set; }
+    }
+
+    public enum EstadoTarjeta
+    {
+        Habilitada,
+        Rechazada
+    }
+
+    public enum AnoExpiracion : int
+    {
+        AnoActual = 2016,
+        AnoProx1 = 2017,
+        AnoProx2 = 2018,
+        AnoProx3 = 2019,
+        AnoProx4 = 2020,
+        AnoProx5 = 2021
+    }
+
+    public enum MesExpiracion
+    {
+        Enero = 1,
+        Febrero,
+        Marzo,
+        Abril,
+        Mayo,
+        Junio,
+        Julio,
+        Agosto,
+        Septiembre,
+        Octubre,
+        Diciembre
+            
     }
 }
